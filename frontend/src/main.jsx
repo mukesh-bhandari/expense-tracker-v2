f5cfe7034b83a3c028fd9c8e1158ea704283b65a
Mukesh <studentmukesh434@gmail.com>
2026-04-05 17:02:26 +0545

    vecrel updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Login from "./pages/login/Login.jsx";
 import Signup from "./pages/signup/Signup.jsx";
 import Room from "./pages/room/Room.jsx";
 import Home from "./pages/home/Home.jsx";
@@ -10,6 +9,7 @@ import Expenses from "./pages/expenses/Expenses.jsx";
 import InviteAccept from "./pages/invite/InviteAccept.jsx";
 import { AuthProvider } from "./contexts/AuthContext.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import Login from "./pages/login/Login.jsx";
 
 const router = createBrowserRouter([
   {
